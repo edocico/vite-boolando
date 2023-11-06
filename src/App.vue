640f81443ce6888a90bfcaaa341f2eeedf0161ad
edocico <138378767+edocico@users.noreply.github.com>
2023-11-06 15:48:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import PageHeader from "./components/PageHeader.vue";
+import PageMain from "./components/PageMain.vue";
+import PageFooter from "./components/PageFooter.vue";
+
+export default {
+  components: {
+    PageHeader,
+    PageMain,
+    PageFooter,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <PageHeader />
+  <PageMain />
+  <PageFooter />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
